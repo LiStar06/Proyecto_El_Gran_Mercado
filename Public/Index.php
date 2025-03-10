b353104bb6aee614bcb381c7dd49aca9a6680db2
Liyan <negritos95@outlook.com>
2025-03-10 11:59:02 -0400

    Pantalla de inicio, Iniciar sesion y Crear cuenta

--- Public/Index.php
+++ Public/Index.php
@@ -3,12 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Imagen Completa</title>
+    <title>El Gran Mercado</title>
     <link rel="stylesheet" href="styles.css"> 
 </head>
 <body>
-    <div class="fondo">
+    <main class="fondo">
+        <nav class = "barra_de_botones">
+            <img id = "Logo" src="logo.png" alt="logo">
+            <h2 id="Nombre_del_juego">El Gran Mercado</h2>
+            <button id = "Jugar">Jugar</button>
+            <button id = "Registrarse">Registrarse</button>
+        </nav>
+        <section id = "Caja_de_contenido">
+            <h1 id="Titulo">¡Bienvenido a El Gran Mercado!</h1>
+            <p id="Texto_de_bienvenida">📈💰 Sumérgete en el mundo de los negocios y las finanzas con El Gran Mercado, el juego donde pondrás a prueba tu capacidad para administrar un negocio, tomar decisiones estratégicas y hacer crecer tu capital.<br>
+                🛒 Compra y vende productos, ajusta precios, invierte con inteligencia y supera los desafíos del mercado en una experiencia educativa y emocionante.<br>
+                🔹 ¿Tienes lo necesario para convertirte en un maestro de las finanzas? Descúbrelo en El Gran Mercado. ¡Juega, aprende y crece económicamente! 🚀
+            </p>
+        </section>
         
-    </div>
+    </main>
 </body>
 </html>
--- Public/p_crear_cuenta.php
+++ Public/p_crear_cuenta.php
@@ -4,11 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Imagen Completa</title>
-    <link rel="stylesheet" href="styles.css"> 
+    <link rel="stylesheet" href="estilos_crear_cuenta.css"> 
 </head>
 <body>
     <div class="fondo">
-        
+        <section id = "Caja_de_contenido">
+            <img id = "Logo" src="logo.png" alt="Logo">
+            <h1 id="Titulo">Bienvenido al menú de Registro</h1>
+
+            <form action="" method="POST">
+                <label for="nombre">Nombre<br></label>
+                <input type="text" id="nombre_del_jugador" name="nombre" required>
+                <br><br>
+                <label for="usuario">Correo electronico <br></label>
+                <input type="text" id="usuario" name="usuario" required>
+                <br><br>
+                <label for="password">Contraseña <br></label>
+                <input type="password" id="password" name="password" required>
+                <br>
+                <button type="submit">Registrarse</button>
+            </form>
+            
+        </section>
     </div>
 </body>
 </html>
